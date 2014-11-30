8f8fc926862441446262e7d9e80618c443a6546a
Will Chiong <will@infomofo.com>
2014-11-30 15:16:19 -0500

    Encapsulate functions for facebook parsing

--- app/controllers/PageInfo.scala
+++ app/controllers/PageInfo.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import org.jsoup.{Connection, Jsoup}
-import org.jsoup.nodes.Element
 import play.Logger
 import play.api.mvc._
 import play.api.libs.json._
@@ -10,70 +9,103 @@ import com.infomofo.oroku.models
 import com.infomofo.oroku.models.json._
 import org.joda.time.DateTime
 
-object PageInfo extends Controller {
-  def getInfoForUrl(url: String) = Action {
+import scala.collection.JavaConverters._
+import scala.util.Try
 
-    val document = Jsoup.connect(url).get()
-    val headElement = document.head()
+private class PageShredder(url: String) {
+  private val document = {
+    val res: Connection.Response = {
+      val response = Jsoup.connect(url)
+        .ignoreHttpErrors(true)
+        .followRedirects(true)
+        .ignoreContentType(true)
+        .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+        .referrer("http://www.google.com")
+        .execute()
+      response match {
+        case x if x.statusCode == 307 =>
+          val newUrl = x.header("Location")
+          if (newUrl != null && newUrl.length > 7) {
+            Jsoup.connect(newUrl).execute()
+          } else {
+            throw new IllegalStateException(s"Invalid url specified by 307 redirect: $newUrl")
+          }
+        case x =>
+          x
+      }
+    }
+    res.parse
+  }
+  private lazy val headElement = document.head
 
-    val ogTitle = headElement.select("meta[property=og:title]").attr("content")
-    val ogType = headElement.select("meta[property=og:title]").attr("content")
+  lazy val openGraphMetadata = {
+    val metaTags = headElement.select("meta[property^=og:]")
 
-    val pageInfo = models.PageInfo(
-      titles = Seq(ogTitle),
-      urls = Seq(url),
+    metaTags.iterator().asScala.foreach {
+      element =>
+        element.attr("property") match {
+          case "og:title" | "og:type" | "og:image" | "og:url" | "og:site_name" | "og:description" =>
+
+          case x =>
+            Logger.warn(s"Unhandled OpenGraph meta tag with property name $x and value ${element.attr("content")}")
+        }
+    }
+    val ogTitle = metaTags.select("meta[property=og:title]").attr("content")
+    val ogType = metaTags.select("meta[property=og:type]").attr("content")
+    val ogImage = metaTags.select("meta[property=og:image]").attr("content")
+    val ogUrl = metaTags.select("meta[property=og:url]").attr("content")
+    val ogSiteName = Try {
+      Some(metaTags.select("meta[property=og:site_name]").attr("content"))
+    }.getOrElse(None)
+    val ogDescription = Try {
+      Some(metaTags.select("meta[property=og:description]").attr("content"))
+    }.getOrElse(None)
+
+    models.OpenGraphMetadata(
+      title = ogTitle,
+      openGraphType = {
+        val openGraphType = models.OpenGraphType(ogType)
+        openGraphType match {
+          case models.OpenGraphType.UNDEFINED(unknownType) =>
+            Logger.warn(s"Page found with an unknown open graph type $unknownType")
+        }
+        openGraphType
+      },
+      image = models.OpenGraphMedia(
+        url = ogImage
+      ),
+      url = ogUrl,
+      siteName = ogSiteName,
+      description = ogDescription
+    )
+  }
+
+  lazy val pageInfo = {
+    models.PageInfo(
+      titles = Seq(openGraphMetadata.title),
+      urls = Seq(openGraphMetadata.url, url).distinct,
       keywords = Nil,
       locations = Nil,
       retrievedAt = new DateTime(),
       site = models.Site(
-        name = "site",
-        siteType = Some("website")
-      )
+        name = openGraphMetadata.siteName.getOrElse(url),
+        siteType = Some(openGraphMetadata.openGraphType.toString)
+      ),
+      openGraphMetadata = Some(openGraphMetadata)
     )
+  }
+}
+
+object PageInfo extends Controller {
+  def getInfoForUrl(url: String) = Action {
+
+    val pageInfo = new PageShredder(url).pageInfo
+
     Ok(Json.toJson(pageInfo))
   }
 
   def getOpenGraphMetadataForUrl(url: String) = Action {
-    val document = {
-      val res: Connection.Response = {
-        val response = Jsoup.connect(url)
-          .ignoreHttpErrors(true)
-          .followRedirects(true)
-          .ignoreContentType(true)
-          .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
-          .referrer("http://www.google.com")
-          .execute()
-        response match {
-          case x if x.statusCode == 307 =>
-            val newUrl = x.header("Location")
-            if (newUrl != null && newUrl.length > 7) {
-              Jsoup.connect(newUrl).execute()
-            } else {
-              throw new IllegalStateException(s"Invalid url specified by 307 redirect: $newUrl")
-            }
-          case x =>
-            x
-        }
-      }
-      res.parse
-    }
-    Logger.debug(s"$document")
-    val headElement = document.head()
-    Logger.debug(s"$headElement")
-
-    val ogTitle = headElement.select("meta[property=og:title]").attr("content")
-    val ogType = headElement.select("meta[property=og:title]").attr("content")
-    val ogImage = headElement.select("meta[property=og:image]").attr("content")
-    val ogUrl = headElement.select("meta[property=og:url]").attr("content")
-
-    val openGraphMetadata = models.OpenGraphMetadata(
-      title = ogTitle,
-      openGraphType = models.OpenGraphType(ogType),
-      image = models.OpenGraphMedia(
-        url = ogImage
-      ),
-      url = ogUrl
-    )
+    val openGraphMetadata = new PageShredder(url).openGraphMetadata
     Ok(Json.toJson(openGraphMetadata))
   }
 }
